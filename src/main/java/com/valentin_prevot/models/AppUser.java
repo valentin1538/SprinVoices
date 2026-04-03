cc7c549d3bef5e99649b740f3070f864409d683a
Valentin1538 <valentin.prevot@gmail.com>
2026-04-03 15:38:45 +0200

    first commit

---
+++ src/main/java/com/valentin_prevot/models/AppUser.java
@@ -0,0 +1,23 @@
+package com.valentin_prevot.invoice_app.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "app_user")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; // "ADMIN" ou "CLIENT"
+}
---
+++ src/main/java/com/valentin_prevot/models/Customer.java
@@ -0,0 +1,26 @@
+package com.valentin_prevot.invoice_app.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    
+    @Column(name = "corporate_name")
+    private String corporateName;
+    
+    private String address;
+
+    // Un client est lié à un compte utilisateur pour se connecter
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private AppUser appUser;
+}
